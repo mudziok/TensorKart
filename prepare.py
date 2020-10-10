09e0b05289b4abef5a789e4f2c8f823a2c734fbe
mudziok <mudziok@gmail.com>
2020-10-10 15:16:57 +0200

    Implement preparing samples from multiple folders

--- prepare.py
+++ prepare.py
@@ -1,34 +1,39 @@
 import numpy as np
 from skimage.transform import resize
 from skimage.io import imread
+import glob
+import os
 
 def resize_image(img):
     im = resize(img, (120, 160, 3))
     im_arr = im.reshape((120, 160, 3))
     return im_arr
 
-print('Preparing data')
+print('Loading folders')
 
-X = []
-Y = []
+paths = glob.glob("data\*\inputs.csv")
 
-image_files = np.loadtxt('data/inputs.csv', delimiter=';', dtype=str, usecols=(0,))
-joystick_values = np.loadtxt('data/inputs.csv', delimiter=';', usecols=(1,2))
+for path in paths:
+    base = os.path.dirname(path)
+    print('Preparing sample: ' + base)
+    
+    X = []
+    Y = []
 
-Y.append(joystick_values)
-Y = np.concatenate(Y)
+    image_files = np.loadtxt(path, delimiter=';', dtype=str, usecols=(0,))
+    joystick_values = np.loadtxt(path, delimiter=';', usecols=(1,2))
 
-for image_file in image_files:
-    image = imread(image_file)
-    vec = resize_image(image)
-    X.append(vec)
+    Y.append(joystick_values)
+    Y = np.concatenate(Y)
 
-X = np.asarray(X)
+    for image_file in image_files:
+        image = imread(image_file)
+        vec = resize_image(image)
+        X.append(vec)
 
-print('Saving data')
-
-np.save("data/X", X)
-np.save("data/Y", Y)
+    X = np.asarray(X)
+    
+    np.save(base + "/X", X)
+    np.save(base + "/Y", Y)
 
 print('Done!')
-
